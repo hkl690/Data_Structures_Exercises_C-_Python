f232cca10a9171ec14d1fe15cc0824f8fcb82f02
Jina W <jina.wilde@my.utsa.edu>
2025-01-28 13:42:34 -0600

    added instructions

--- Data_Structures_Exercises_C#_Python/2_Queue/C#_Queue/Program.cs
+++ Data_Structures_Exercises_C#_Python/2_Queue/C#_Queue/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Xml.Linq;
 
 namespace Data_Structures_Exercises_C__Python._2_Queue.C__Queue
 {
@@ -10,3 +13,11 @@ namespace Data_Structures_Exercises_C__Python._2_Queue.C__Queue
     {
     }
 }
+
+//implement a menu based program with the following array-based queue functions:
+
+//add(an element to the end of queue)
+//delete(an element from the front of queue)
+//show(all elements, starting at first, ending at last)
+//count(total number of elements)
+//clear(initialize queue)
