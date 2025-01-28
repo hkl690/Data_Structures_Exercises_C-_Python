96223978c754074dbb9dddb7842321f163ddc4dd
Jina W <jina.wilde@my.utsa.edu>
2025-01-28 12:07:05 -0600

    Add project files.

---
+++ Data_Structures_Exercises_C#_Python/1_List/C#_List/ListManager.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_Exercises
+{
+    public class ListManager
+    {
+        private int[] list;
+
+        // Constructor to initialize the list
+        public ListManager()
+        {
+            list = new int[0];
+        }
+
+        /// <summary>
+        /// Method to add an element to the end of the list (case 1)
+        /// </summary>  
+        public void AddToEnd()
+        {
+            Console.WriteLine("Enter the number to add to the list: ");
+            int newElement;
+
+            // Validate user input
+            while (!int.TryParse(Console.ReadLine(), out newElement))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number: ");
+            }
+
+            // Create a new list with an extra space
+            int[] newList = new int[list.Length + 1];
+
+            // Copy the elements from the old list to the new list
+            for (int i = 0; i < list.Length; i++)
+            {
+                newList[i] = list[i];
+            }
+
+            // Add the new element to the end of the new list
+            newList[list.Length] = newElement;
+
+            // Update the reference to the new list
+            list = newList;
+
+            Console.WriteLine($"Element {newElement} added to the end of the list.");
+        }
+
+        /// <summary>
+        /// Method to insert an element at a given location (case 2)
+        /// </summary>
+        public void InsertElement(int index, int newValue)
+        {
+            if (index < 0 || index > list.Length)
+            {
+                Console.WriteLine("Invalid index.");
+                return;
+            }
+            // Create a new list with an extra space
+            int[] newList = new int[list.Length + 1];
+
+            // Copy the elements from the old list to the new list
+            for (int i = 0; i < index; i++)
+            {
+                newList[i] = list[i];
+            }
+            // Insert the new element at the specified index
+            newList[index] = newValue;
+
+            // Copy the remaining elements from the old list to the new list
+            for (int i = index; i < list.Length; i++)
+            {
+                newList[i + 1] = list[i];
+            }
+            // Update the reference to the new list
+            list = newList;
+            Console.WriteLine($"Inserted element {newValue} at index {index}.");
+        }
+
+        /// <summary>
+        /// Method to delete the element at a given location (case 3)
+        /// </summary>
+        public void DeleteElement(int index)
+        {
+            if (index < 0 || index >= list.Length)
+            {
+                Console.WriteLine("Invalid index.");
+                return;
+            }
+            // Create a new list with one less space
+            int[] newList = new int[list.Length - 1];
+
+            for (int i = 0; i < index; i++)
+            {
+                newList[i] = list[i];
+            }
+            // Copy elements after the deleted index
+            for (int i = index + 1; i < list.Length; i++)
+            {
+                newList[i - 1] = list[i];
+            }
+            // Update the reference to the new list
+            list = newList;
+
+            Console.WriteLine($"Element at index {index} has been deleted.");
+
+        }
+
+        /// <summary>
+        /// Method to display the list (case 4)
+        /// </summary>
+        public void DisplayList()
+        {
+            if (list.Length == 0)
+            {
+                Console.WriteLine("The list is empty.\n");
+                return;
+            }
+            /*
+            Console.WriteLine("These are the elements in your list.");
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.WriteLine($"Index {i}: {list[i]}");
+            }*/
+            Console.WriteLine("List elements: [" + string.Join(", ", list) + "]\n");
+        }
+
+        /// <summary>
+        /// Method to display the count of the total number of elements in the list (case 5)
+        /// </summary>
+        public void DisplayCount()
+        {
+            int count = list.Length;
+            Console.WriteLine($"The list contains {count} elements.");
+        }
+
+        /// <summary>
+        /// Method to clear (initialize) the list (case 6)
+        /// </summary> 
+        public void ClearList()
+        {
+            list = new int[0];
+            Console.WriteLine("List has been cleared.");
+        }
+
+    }
+}
---
+++ Data_Structures_Exercises_C#_Python/1_List/C#_List/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Data_Structures_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListManager listExercise = new ListManager();
+            string UserInput1;
+            int Option = 0;
+            bool IsRunning = true;
+
+            // Note: to improve this main method, you could add a method to display the menu
+
+            // Loop for both menu display and validation
+            while (IsRunning)
+            {
+                Console.WriteLine("A menu of options: \n");
+                Console.WriteLine("Press 1 to add to the end of the list.\n" +
+                    "Press 2 to insert an element at a given location.\n" +
+                    "Press 3 to delete an element from a given location.\n" +
+                    "Press 4 to display all of the list elements.\n" +
+                    "Press 5 to count the total number of elements.\n" +
+                    "Press 6 to clear (initialize) an list.\n" +
+                    "Press 7 to exit the program.\n");
+
+                Console.WriteLine("Please enter a number between 1 and 7: ");
+                UserInput1 = Console.ReadLine();
+
+                // Try to parse the input to an integer
+                if (int.TryParse(UserInput1, out Option) && Option >= 1 && Option <= 7)
+                {
+                    Console.WriteLine($"You entered: {Option}");
+
+                    // Switch statement to handle menu options
+                    switch (Option)
+                    {
+                        case 1:
+                            listExercise.AddToEnd();
+                            listExercise.DisplayList();
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter the index where you want to insert the element: ");
+                            int InsertIndex = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter the value you want to insert: ");
+                            int InsertValue = Convert.ToInt32(Console.ReadLine());
+                            listExercise.InsertElement(InsertIndex, InsertValue);
+                            listExercise.DisplayList();
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter the index of the element you want to delete: ");
+                            int DeleteIndex = Convert.ToInt32(Console.ReadLine());
+                            listExercise.DeleteElement(DeleteIndex);
+                            listExercise.DisplayList();
+                            break;
+                        case 4:
+                            listExercise.DisplayList();
+                            break;
+                        case 5:
+                            listExercise.DisplayCount();
+                            listExercise.DisplayList();
+                            break;
+                        case 6:
+                            listExercise.ClearList();
+                            listExercise.DisplayList();
+                            break;
+                        case 7:
+                            IsRunning = false;
+                            Console.WriteLine("Exiting the program.");
+                            break;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a number between 1 and 7.");
+                }
+            }
+        }
+    }
+}
+
